2881f2d733ae578c2f9f08166f0f5fa21d2ca128
ggo <goncalves_gilles@hotmail.fr>
2022-03-23 16:19:54 +0100

    [FIX]: fix error divide by zero in stock picking computed description

---
+++ revivre-addons/revivre_sale/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Revivre Sale",
+    'summary': 'Custom Sale Management',
+    'description': 'Custom Sale Management',
+    'version': '14.0.0.0.0',
+    'author': 'ArkeUp SAS',
+    'website': 'https://arkeup.com',
+    'depends': [
+        'merge_similar_packaging_product',
+    ],
+    'data': [
+        # data
+        # security
+        # views
+        # wizard
+        # reports
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
