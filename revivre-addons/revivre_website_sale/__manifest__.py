2f182b9b6abb7fdcae725c566d6573d8726be173
ggo <goncalves_gilles@hotmail.fr>
2022-03-24 09:49:02 +0100

    [ADD]:add package quantity in website cart

---
+++ revivre-addons/revivre_website_sale/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Revivre Website Sale",
+    'summary': 'Custom Website Sale Management',
+    'description': 'Custom Website Sale Management',
+    'version': '14.0.0.0.0',
+    'author': 'ArkeUp SAS',
+    'website': 'https://arkeup.com',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        # data
+        # security
+        # views
+        # wizard
+        # reports
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
